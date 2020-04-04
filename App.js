acfb0396900ec6a3d60d690d431fa5a3e284cbdf
devdattakhoche <dkhoche70@gmail.com>
2020-04-04 19:38:16 +0530

    Code Clean up and Indentation

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
-import { NavigationContainer } from '@react-navigation/native';
-import * as React from 'react';
-import Map from './Components/MapView';
-import { BottomTabBar } from '@react-navigation/bottom-tabs'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import { Provider as PaperProvider, DefaultTheme, IconButton , Button} from 'react-native-paper';
-import TableView from './Components/TableView';
-import { Ionicons , Feather , AntDesign } from '@expo/vector-icons'
-import CityView from './Components/CityView';
-import Home from './Components/Home';
-
+import { NavigationContainer } from "@react-navigation/native";
+import * as React from "react";
+import Map from "./Components/MapView";
+import { BottomTabBar } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StyleSheet, View } from "react-native";
+import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
+import TableView from "./Components/TableView";
+import { Ionicons, Feather, AntDesign } from "@expo/vector-icons";
+import CityView from "./Components/CityView";
+import Home from "./Components/Home";
 
 const theme = {
   ...DefaultTheme,
@@ -22,77 +21,87 @@ const theme = {
 };
 const Tab = createBottomTabNavigator();
 
-
 export default function App() {
   return (
-    <PaperProvider  theme = {DefaultTheme}>
- <View style = {{flex:1}}>
-    
-      
-    
-  
-    <NavigationContainer >
-    <Tab.Navigator initialRouteName = 'Home' tabBarOptions={{activeTintColor:'black',inactiveTintColor:'gray'}} tabBar={props => <BottomTabBar {...props} state={{...props.state, routes: props.state.routes.slice(0,3)}}></BottomTabBar>}> 
-    <Tab.Screen name="Home" component={Home} options={{
-         
-         tabBarIcon: ({ focused} ) => {
-            if(focused) 
-            {
-              return <AntDesign name='home' size={20} color = '#4c8bf5'  />
-            }
-            else{
-              return <AntDesign name='home' size={20} color = 'gray'  /> 
-            }
-          }
-           
-         
-        
-        }} ></Tab.Screen>
-      <Tab.Screen name="Map" component={Map}   options={{
-          tabBarLabel: 'Map View',
-          tabBarIcon: ({ focused} ) => {
-            if(focused) 
-            {
-              return <Feather name='map' size={20} color = '#4c8bf5' />
-            }
-            else{
-              return <Feather name='map' size={20} color = 'gray' /> 
-            }
-          }
-           
-            
-        
-        }}
-        ></Tab.Screen>
-      <Tab.Screen name="Country View" component={TableView} options={{
-         
-         tabBarIcon: ({ focused} ) => {
-            if(focused) 
-            {
-              return <Ionicons name='ios-list-box' size={20} color = '#4c8bf5'  />
-            }
-            else{
-              return <Ionicons name='ios-list-box' size={20} color = 'gray'  /> 
-            }
-          }
-           
-         
-        
-        }} ></Tab.Screen>
-        
-      <Tab.Screen name="CityView" component={CityView} tabBarVisible = {false} ></Tab.Screen>
-    </Tab.Navigator>
-    </NavigationContainer>
-    
-    </View>
-  </PaperProvider>
+    <PaperProvider theme={DefaultTheme}>
+      <View style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Tab.Navigator
+            initialRouteName="Home"
+            tabBarOptions={{
+              activeTintColor: "black",
+              inactiveTintColor: "gray"
+            }}
+            tabBar={props => (
+              <BottomTabBar
+                {...props}
+                state={{
+                  ...props.state,
+                  routes: props.state.routes.slice(0, 3)
+                }}
+              ></BottomTabBar>
+            )}
+          >
+            <Tab.Screen
+              name="Home"
+              component={Home}
+              options={{
+                tabBarIcon: ({ focused }) => {
+                  if (focused) {
+                    return <AntDesign name="home" size={20} color="#4c8bf5" />;
+                  } else {
+                    return <AntDesign name="home" size={20} color="gray" />;
+                  }
+                }
+              }}
+            ></Tab.Screen>
+            <Tab.Screen
+              name="Map"
+              component={Map}
+              options={{
+                tabBarLabel: "Map View",
+                tabBarIcon: ({ focused }) => {
+                  if (focused) {
+                    return <Feather name="map" size={20} color="#4c8bf5" />;
+                  } else {
+                    return <Feather name="map" size={20} color="gray" />;
+                  }
+                }
+              }}
+            ></Tab.Screen>
+            <Tab.Screen
+              name="Country View"
+              component={TableView}
+              options={{
+                tabBarIcon: ({ focused }) => {
+                  if (focused) {
+                    return (
+                      <Ionicons name="ios-list-box" size={20} color="#4c8bf5" />
+                    );
+                  } else {
+                    return (
+                      <Ionicons name="ios-list-box" size={20} color="gray" />
+                    );
+                  }
+                }
+              }}
+            ></Tab.Screen>
 
+            <Tab.Screen
+              name="CityView"
+              component={CityView}
+              tabBarVisible={false}
+            ></Tab.Screen>
+          </Tab.Navigator>
+        </NavigationContainer>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFCFF',
+    backgroundColor: "#FFFCFF"
   }
 });
